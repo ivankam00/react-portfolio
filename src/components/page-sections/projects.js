1f901629fe06879809b5b15376c1b02fe00310d6
ivankam00 <ivank00@yahoo.ca>
2019-10-26 13:37:30 -0600

    Saturday minor update

--- src/components/page-sections/projects.js
+++ src/components/page-sections/projects.js
@@ -21,7 +21,7 @@ export default class Sidebar extends Component {
                     <ImageInfo
                         image={<img src="./images/fcqns.jpg" alt="FCQNS.Tech Inc." />}
                         title="FCQNS.Tech Website"
-                        subtitle={<span>Build in November 2017 for URL <a href="https://fcqns.tech" target="_blank">https://fcqns.tech</a></span>}
+                        subtitle={<span>Built in November 2017 for URL <a href="https://fcqns.tech" target="_blank">https://fcqns.tech</a></span>}
                         description={<p>When I was working at FCQNS.Tech Inc., I was placed in charge of coding the front-end layout of the
                             company's website. There was no wirefreame design for layout, I just built it based on what my boss imagined, having
                             him show me examples of sections on other websites.<br/> Later on, I would also be adding addition pages and
@@ -30,10 +30,19 @@ export default class Sidebar extends Component {
                     <ImageInfo
                         image={<img src="./images/lumenz.jpg" alt="Lumenz" />}
                         title="Lumenz Website"
-                        subtitle={<span>Build in June 2018 for URL <a href="https://lumenz.ca" target="_blank">https://lumenz.ca</a></span>}
+                        subtitle={<span>Built in June 2018 for URL <a href="https://lumenz.ca" target="_blank">https://lumenz.ca</a></span>}
                         description={<p>I was tasked to make an exact duplicate of the client's existing website and rebuild it so that there
                             are no security or functionality issues when porting them into the Solar Store Network service offered by
                             FCQNS.Tech Inc.</p>}
+                    />
+                    <ImageInfo
+                        image={<img src="./images/contractors_contact.jpg" alt="ContractorsContact" />}
+                        title="ContractorContacts Website"
+                        subtitle={<span>Built in August 2018 for URL <a href="https://contractorscontact.com" target="_blank">https://contractorscontact.com</a></span>}
+                        description={<p>Tasked with building a platform to contract out solar panel installation that cannot be handle by
+                            the solar retail company, where the products were purchased. I utilized 3rd party applications, along with some
+                            custom back-end coding, to develop an online platform where solar store retailers on the FCQNS.Tech Solar Store
+                            Network could post installation jobs for licensed solar installation contractors.</p>}
                     />
                 </div>
             </section> 
